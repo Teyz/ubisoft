831c7de05d754c1b856116f829037cab75d8543a
teyz <contact@bastienrigaud.fr>
2021-01-24 21:56:47 +0100

    Fix: link for dashboard

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ app.get('/lowan', function (req, res) {
     res.sendFile(path.join(__dirname + '/lowan.html'));
 });
 
-app.get('/dashboard', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/dashboard.html'));
 });
 
